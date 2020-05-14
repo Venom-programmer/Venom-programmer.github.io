ab6753061b2e2273fbfefa2c16489a2e80ad6563
Venom-programmer <63306942+Venom-programmer@users.noreply.github.com>
2020-05-14 11:27:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ function getDate() {
 
 
 
-
-
 function myFunction() {
   var q1 = document.getElementById('1').checked;
   var q2 = document.getElementById('2').checked;
@@ -23,8 +21,32 @@ function myFunction() {
   var q6 = document.getElementById('6').checked;
   var q7 = document.getElementById('7').checked;
 
-  if (q1 == true && q5 == true) {
+  if (q3 == true && q5 == true) {
     document.getElementById('o').href = 'X-Max.html';
     document.getElementById('o').innerHTML = 'X-Max';
+  } else if (q4 == true && q7 == true) {
+    document.getElementById('o').href = 'Ultimaker_3.html';
+    document.getElementById('o').innerHTML = 'Ultimaker 3';
+  } else if (q1 == true && q5 == true) {
+    document.getElementById('o').href = 'creality_CR.html';
+    document.getElementById('o').innerHTML = 'Creality CR 10';
+  } else if (q1 == true && q7 == true) {
+    document.getElementById('o').href = 'creality_CR.html';
+    document.getElementById('o').innerHTML = 'Creality CR 10';
+  } else if (q3 == true && q7 == true) {
+    document.getElementById('o').href = 'MakerBot_Replicator+.html';
+    document.getElementById('o').innerHTML = 'MakerBot Replicator +';
+  } else if (q1 == true && q6 == true) {
+    document.getElementById('o').href = 'creality_resin.html';
+    document.getElementById('o').innerHTML = 'Creality LD-002R LCD';
+  } else if (q4 == true && q6 == true) {
+    document.getElementById('o').href = 'form_3.html';
+    document.getElementById('o').innerHTML = 'Formlabs Form 3';
+  } else if (q4 == true && q5 == true) {
+    document.getElementById('o').href = 'markforged_2.html';
+    document.getElementById('o').innerHTML = 'Markforged Mark two';
+  } else {
+    document.getElementById('o').href = '';
+    document.getElementById('o').innerHTML = '';
   }
 }
